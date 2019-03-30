c976ef1830a859405b838e8be13a5dcccec4767b
Orkhan Huseynli <o.huseynli@asan.gov.az>
2019-03-30 09:16:41 +0400

    added before each

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import App from "../App";
-import CommentBox from "../CommentBox";
-import CommentList from "../CommentList";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+let wrapper;
+
+beforeEach(function() {
+  wrapper = shallow(<App />);
+});
 
 it("shows a comment box", function() {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
 it("shows the comment list", function() {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentList).length).toEqual(1);
 });
