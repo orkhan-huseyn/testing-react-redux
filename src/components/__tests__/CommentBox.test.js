7edfb7aac2138cd917f047806bf61a41591eaa5f
Orkhan Huseynli <o.huseynli@asan.gov.az>
2019-03-31 09:47:32 +0400

    comment box test ready

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,8 +16,29 @@ describe("<CommentBox />", function() {
   });
 
   it("has a textarea and a button", function() {
-    expect(wrapper.find('textarea').length).toEqual(1);
-    expect(wrapper.find('button').length).toEqual(1);
+    expect(wrapper.find("textarea").length).toEqual(1);
+    expect(wrapper.find("button").length).toEqual(1);
+  });
+
+  describe("<form>", function() {
+
+    beforeEach(function() {
+      wrapper.find("textarea").simulate("change", {
+        target: { value: "new comment" }
+      });
+      wrapper.update();
+    });
+
+    it("has a text area that users can type is", function() {
+      expect(wrapper.find("textarea").prop("value")).toEqual("new comment");
+    });
+
+    it("clears the textarea after form is submitted", function() {
+      wrapper.find("form").simulate("submit");
+      wrapper.update();
+      expect(wrapper.find("textarea").prop("value")).toEqual("");
+    });
+
   });
 
 });
