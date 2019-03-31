a187613c35cb473cc08507fec61101ec4f0f2a68
Orkhan Huseynli <o.huseynli@asan.gov.az>
2019-03-31 21:16:39 +0400

    CommetnList first test

--- src/Root.js
+++ src/Root.js
@@ -3,9 +3,10 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "reducers";
 
-export default function({ children }) {
+export default function({ children, initialState = {} }) {
+
   return (
-    <Provider store={createStore(rootReducer, {},
+    <Provider store={createStore(rootReducer, initialState,
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )}>
       {children}
--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -3,3 +3,25 @@ import { mount } from "enzyme";
 
 import Root from "Root";
 import CommentList from "components/CommentList";
+
+describe("<CommentList />", function() {
+
+  let wrapper;
+
+  beforeEach(function() {
+    const initialState = {
+      comments: ["Comment 1", "Comment 2"]
+    };
+
+    wrapper = mount(
+      <Root initialState={initialState}>
+        <CommentList />
+      </Root>
+    );
+  });
+
+  it("creates one <li> per comment", function() {
+    expect(wrapper.find("li").length).toEqual(2);
+  });
+
+});
