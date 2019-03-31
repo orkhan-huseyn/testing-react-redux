1e3cdcb9e1e614537bce6c28256860f68042395f
Orkhan Huseynli <o.huseynli@asan.gov.az>
2019-03-31 10:36:55 +0400

    written test for commentsReducer

---
+++ src/reducers/__tests__/comments.test.js
@@ -0,0 +1,17 @@
+import { commentsReducer } from "reducers/comments";
+import { saveComment } from "actions";
+
+describe("Comments Reducer", function() {
+
+  it("handles actions of type SAVE_COMMENT", function() {
+    const action = saveComment("new comment");
+    const newState = commentsReducer([], action);
+    expect(newState).toEqual(["new comment"]);
+  });
+
+  it("handles actions with unknown type", function() {
+    const newState = commentsReducer([], { type: "RANDOM_ACTION" });
+    expect(newState).toEqual([]);
+  });
+
+});
