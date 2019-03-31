8f6bd9fc116ae0456a94a15042ed4189e0c27306
Orkhan Huseynli <o.huseynli@asan.gov.az>
2019-03-31 10:43:23 +0400

    tested action creators

---
+++ src/actions/__tests__/index.test.js
@@ -0,0 +1,15 @@
+import { SAVE_COMMENT } from "actions/types";
+import { saveComment } from "actions";
+
+describe("saveComment", function() {
+
+  it("has the correct type", function() {
+    const action = saveComment();
+    expect(action.type).toEqual(SAVE_COMMENT);
+  });
+
+  it("has the correct payload", function() {
+    const action = saveComment("new comment");
+    expect(action.payload).toEqual("new comment");
+  });
+});
